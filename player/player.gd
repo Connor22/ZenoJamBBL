3aff95ce10de59fff4ce65c44baac92b2b85331f
M0USEU <135737110+M0USEU@users.noreply.github.com>
2024-04-29 19:29:04 +0100

    Adjusted the project resolution/scale

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,8 @@
 class_name Player
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 250.0
+const SHIELD_BASH_SPEED = 1200
 var _shield_bash_direction = Vector2.ZERO
 
 func _ready():
@@ -11,13 +12,13 @@ func _ready():
 
 func _physics_process(delta):
 	if !$ShieldBashTimer.is_stopped():
-		velocity = _shield_bash_direction * 1800
+		velocity = _shield_bash_direction * SHIELD_BASH_SPEED
 	elif Input.is_action_just_pressed("shield_bash"):
 		$ImpactShape.monitorable = true
 		$ImpactShape.visible = true
 		_shield_bash_direction = (get_global_mouse_position() - position).normalized()
 		$Pivot.modulate = Color(1000,1000,1000)
-		velocity = _shield_bash_direction * 1800
+		velocity = _shield_bash_direction * SHIELD_BASH_SPEED
 		$ShieldBashTimer.start()
 	else:
 		# Get the input direction and handle the movement
