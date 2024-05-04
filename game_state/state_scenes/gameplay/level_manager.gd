140472e96f2a0ab60fae6a43a27ae7958ffd92b7
M0USEU <135737110+M0USEU@users.noreply.github.com>
2024-05-04 17:00:40 +0100

    Added Level transitions, Level buttons, Level Exit, Basic UI Improvements

--- game_state/state_scenes/gameplay/level_manager.gd
+++ game_state/state_scenes/gameplay/level_manager.gd
@@ -1,11 +1,20 @@
 class_name LevelManager
 extends CanvasLayer
 
-var _current_level: int = 0
+var _current_index: int = 0
 
 func initiate_level(index: int):
-	_current_level = index
+	# Initiate this level
+	_current_index = index
 	var main = $/root/Main as Main
 	var packed_level = main.packed_level_array[index] as PackedScene
-	var level = packed_level.instantiate()
-	add_child(level)
+	var level = packed_level.instantiate() as Level
+	add_child.call_deferred(level)
+	# When the player completes this level, go to the next
+	level.level_exit.connect("level_complete", next_level)
+
+func next_level():
+	for node in get_children():
+		node.queue_free()
+	_current_index += 1
+	initiate_level(_current_index)
