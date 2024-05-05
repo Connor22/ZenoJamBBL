6348494f10c25add8436d7b7e96cb65f3b67a171
M0USEU <135737110+M0USEU@users.noreply.github.com>
2024-05-05 06:53:31 +0100

    Art to levels, main menu art, coffee sprite

--- game_state/main_menu_state.gd
+++ game_state/main_menu_state.gd
@@ -4,8 +4,6 @@ extends GameState
 func _enter(init_data: ={}):
 	current_scene = state_scene.instantiate()
 	add_child.call_deferred(current_scene)
-	# Assign events to state transition
-	current_scene.get_node("GUI/Menu/Quit").pressed.connect($/root/Main.request_quit)
 
 func _exit(init_data: ={}):
 	current_scene.queue_free()
